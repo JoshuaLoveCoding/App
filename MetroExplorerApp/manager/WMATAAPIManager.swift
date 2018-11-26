df504ae1e2f2ffb4a5eaac56529b76fb8f0e02c4
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-11-26 10:56:09 -0500

    sixth commit

---
+++ MetroExplorerApp/manager/WMATAAPIManager.swift
@@ -0,0 +1,9 @@
+//
+//  WMATAAPIManager.swift
+//  MetroExplorerApp
+//
+//  Created by Joshua on 11/25/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+import Foundation
---
+++ MetroExplorerApp/models/WMATAResponse.swift
@@ -0,0 +1,9 @@
+//
+//  WMATAResponse.swift
+//  MetroExplorerApp
+//
+//  Created by Joshua on 11/25/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+import Foundation
